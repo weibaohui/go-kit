82805f226df04e621d1688789236f7afbe55ac42
weibaohui <weibaohui@yeah.net>
2018-04-13 13:20:58 +0800

    增加常用方法

--- datekit/datekit.go
+++ datekit/datekit.go
@@ -3,9 +3,23 @@ package datekit
 import "time"
 
 const (
-	normalLayout = "2006-01-02 15:04:05"
+	normalLayout         = "2006-01-02 15:04:05"
+	yyyyMMddHHmmssLayout = "20060102150405"
+	yyyyMMddLayout       = "20060102"
+	HHmmssLayout         = "150405"
 )
 
+func YYYYMMddHHmmss() string {
+	return time.Now().Format(yyyyMMddHHmmssLayout)
+}
+
+func YYYYMMdd() string {
+	return time.Now().Format(yyyyMMddLayout)
+}
+
+func HHmmss() string {
+	return time.Now().Format(HHmmssLayout)
+}
 func NowString() string {
 	return time.Now().Format(normalLayout)
 }
