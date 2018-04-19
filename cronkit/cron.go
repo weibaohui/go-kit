bcd49f5b9c88f6ff9f9b44bd7996f2974d9ae448
weibaohui <weibaohui@yeah.net>
2018-04-19 11:31:17 +0800

    fmt

--- cronkit/cron.go
+++ cronkit/cron.go
@@ -179,27 +179,27 @@ func (t *Task) GetPrev() time.Time {
 // SetCron some signals：
 //       *： any time
 //       ,：　 separate signal
-//　　    －：duration
+// 　　    －：duration
 //       /n : do as n times of time duration
-/////////////////////////////////////////////////////////
-//	0/30 * * * * *                        every 30s
-//	0 43 21 * * *                         21:43
-//	0 15 05 * * * 　　                     05:15
-//	0 0 17 * * *                          17:00
-//	0 0 17 * * 1                           17:00 in every Monday
-//	0 0,10 17 * * 0,2,3                   17:00 and 17:10 in every Sunday, Tuesday and Wednesday
-//	0 0-10 17 1 * *                       17:00 to 17:10 in 1 min duration each time on the first day of month
-//	0 0 0 1,15 * 1                        0:00 on the 1st day and 15th day of month
-//	0 42 4 1 * * 　 　                     4:42 on the 1st day of month
-//	0 0 21 * * 1-6　　                     21:00 from Monday to Saturday
-//	0 0,10,20,30,40,50 * * * *　           every 10 min duration
-//	0 */10 * * * * 　　　　　　              every 10 min duration
-//	0 * 1 * * *　　　　　　　　               1:00 to 1:59 in 1 min duration each time
-//	0 0 1 * * *　　　　　　　　               1:00
-//	0 0 */1 * * *　　　　　　　               0 min of hour in 1 hour duration
-//	0 0 * * * *　　　　　　　　               0 min of hour in 1 hour duration
-//	0 2 8-20/3 * * *　　　　　　             8:02, 11:02, 14:02, 17:02, 20:02
-//	0 30 5 1,15 * *　　　　　　              5:30 on the 1st day and 15th day of month
+// ///////////////////////////////////////////////////////
+// 	0/30 * * * * *                        every 30s
+// 	0 43 21 * * *                         21:43
+// 	0 15 05 * * * 　　                     05:15
+// 	0 0 17 * * *                          17:00
+// 	0 0 17 * * 1                           17:00 in every Monday
+// 	0 0,10 17 * * 0,2,3                   17:00 and 17:10 in every Sunday, Tuesday and Wednesday
+// 	0 0-10 17 1 * *                       17:00 to 17:10 in 1 min duration each time on the first day of month
+// 	0 0 0 1,15 * 1                        0:00 on the 1st day and 15th day of month
+// 	0 42 4 1 * * 　 　                     4:42 on the 1st day of month
+// 	0 0 21 * * 1-6　　                     21:00 from Monday to Saturday
+// 	0 0,10,20,30,40,50 * * * *　           every 10 min duration
+// 	0 */10 * * * * 　　　　　　              every 10 min duration
+// 	0 * 1 * * *　　　　　　　　               1:00 to 1:59 in 1 min duration each time
+// 	0 0 1 * * *　　　　　　　　               1:00
+// 	0 0 */1 * * *　　　　　　　               0 min of hour in 1 hour duration
+// 	0 0 * * * *　　　　　　　　               0 min of hour in 1 hour duration
+// 	0 2 8-20/3 * * *　　　　　　             8:02, 11:02, 14:02, 17:02, 20:02
+// 	0 30 5 1,15 * *　　　　　　              5:30 on the 1st day and 15th day of month
 func (t *Task) SetCron(spec string) {
 	t.Spec = t.parse(spec)
 }
@@ -389,7 +389,7 @@ func dayMatches(s *Schedule, t time.Time) bool {
 // StartTask start all tasks
 func StartTask() {
 	if isstart {
-		//If already started， no need to start another goroutine.
+		// If already started， no need to start another goroutine.
 		return
 	}
 	isstart = true
